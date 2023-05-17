09ea13052bca84482c315205e2e064fb5dd58af4
unknown <chaneil2@naver.com>
2023-05-17 16:26:15 +0900

    [ADD]title_database

---
+++ data_base.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pymongo import MongoClient
+
+# MongoDB 연결 설정
+client = MongoClient('mongodb+srv://chaneil2:k8909@jbchat.abd4i2a.mongodb.net/')  # MongoDB 서버 주소와 포트 설정
+db = client['chat']  # 데이터베이스 선택
+collection = db['titles']  # 컬렉션 선택
+
+# CSV 파일 읽기
+data = pd.read_csv('./학사공지 리스트.csv')
+
+# 데이터 변환 및 MongoDB에 삽입
+records = data.to_dict('records')  # 각 행을 사전 형태로 변환
+collection.delete_many({}) #데이터 초기화
+collection.insert_many(records)  # MongoDB에 데이터 삽입
+
+# 연결 종료
+client.close()
--- jb _ex.py
+++ jb _ex.py
@@ -1,15 +1,20 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
+import os
 x=1
-for x in range(1,4):
-    url = "https://www.joongbu.ac.kr/board.es?mid=a10302000000&bid=0008&&nPage="+str(x)
+filename = "학사공지 리스트.csv"
+if os.path.exists(filename):
+    with open(filename,"w",encoding="utf-8-sig",newline=""):
+        pass
+for x in range(1,3):
+    url = "https://www.joongbu.ac.kr/board.es?mid=a10301000000&bid=0007&nPage="+str(x)
 
     filename = "학사공지 리스트.csv"
     f = open(filename, "a", encoding="utf-8-sig", newline="")
     writer = csv.writer(f)
 
-    colums_name = ["번호","제목"]
+    colums_name = ["페이지","번호","제목","url"]
 
     writer.writerow(colums_name)
 
@@ -19,15 +24,16 @@ for x in range(1,4):
     soup = BeautifulSoup(res.text,"lxml")
     targetbox = soup.find('section',attrs={"id":"content_detail"})
     targetbox2 = targetbox.find('div',attrs={"id":"listView"})
-    print(targetbox2)
+    #print(targetbox2)
     if targetbox is not None:
         targets = targetbox2.find_all('a')
         i=1
 
         for target in targets:
             title=target.get("title")
-            print(f"{str(i)}: {title}")
-            data = [str(i),title]
+            turl=target.get("href")
+            #print(f"{str(i)}: {title}")
+            data = ['page'+str(x),'title:'+str(i),title,'https://www.joongbu.ac.kr'+turl]
             writer.writerow(data)
             i +=1
 
@@ -35,5 +41,6 @@ for x in range(1,4):
             f.write(targetbox.prettify())
     else:
         print("목록을 찾을 수 없습니다.")
+    x +=1 
     
 f.close()
